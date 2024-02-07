395613e3473eae132ed0fe6663520424759e1ecb
benAmi <minebenking@gmail.com>
2024-02-07 14:37:00 +0200

    no target

--- target/debug/deps/scanner-be06ddf920f8be9b.d
+++
@@ -1,8 +0,0 @@
-/home/benami/Code/rython/target/debug/deps/libscanner-be06ddf920f8be9b.rmeta: scanner/src/lib.rs scanner/src/token.rs
-
-/home/benami/Code/rython/target/debug/deps/libscanner-be06ddf920f8be9b.rlib: scanner/src/lib.rs scanner/src/token.rs
-
-/home/benami/Code/rython/target/debug/deps/scanner-be06ddf920f8be9b.d: scanner/src/lib.rs scanner/src/token.rs
-
-scanner/src/lib.rs:
-scanner/src/token.rs:
